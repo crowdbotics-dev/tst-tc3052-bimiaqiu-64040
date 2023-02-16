6c0941039d28357cd73572922777b2de067df275
Crowdbotics <engineering@crowdbotics.com>
2023-02-16 06:18:46 +0000

    Add Frame 1 (5:1) 69

---
+++ screens/Frame1(5:1)69/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Frame1(5:1)69 = () => {
+  return <View style={_styles.Frame1(5:1)69}>
+      </View>;
+};
+
+export default Frame1(5:1)69;
+
+const _styles = StyleSheet.create({
+  Frame1(5:1)69: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
